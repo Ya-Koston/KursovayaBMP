acb1db2856d2340ab44318d0cfca2e719348938a
Ya-Koston <Ya_Koston@mail.ru>
2012-12-11 21:23:20 +0400

    Update BMP.cpp

--- BMP.cpp
+++ BMP.cpp
@@ -4,7 +4,7 @@
 #include <fstream>
 using namespace std;
 
-#pragma pack(push, 2)//зададим выравнивание по наименьшему из типов
+#pragma pack(push, 2)//зададим выравнивание структуры по наименьшему из типов
 
 struct BMP {//создадим общую стуктуру картинки
 	char B,M;//тип
@@ -26,48 +26,49 @@ struct BMP {//создадим общую стуктуру картинки
 #pragma pack(pop)
 
 int main () {
+	cout<<"Please, enter the path of the picture to inversion: ";
 	BMP picture;
 
 	//блок считывания файла от пользователя
-	char* file_name;
-	file_name = (char *) calloc(256, sizeof(char));//выделим память под имя
-	cin>>file_name;
-	FILE *stream = fopen(file_name, "r+");
-	free(file_name);//очистим память взятую под имя
-
-	if (stream == NULL) {
-		cout<<"File not found\n";
-		exit(1);
-	}
+	char* infile_name;
+	infile_name = (char *) calloc(256, sizeof(char));//выделим память под имя
+	cin>>infile_name;
+	FILE *stream = fopen(infile_name, "r+");
+	free(infile_name);//очистим память взятую под имя
 	
 	//проверка типа файла
 	fread(&picture, sizeof(BMP), 1, stream);
-	if ((picture.B != 'B') || (picture.M != 'M')){
-		cout<<"File isn't BitMap\n";
-		exit(-1);}
-	if (picture.biBitCount != 24){
-		cout<<"File isn't 24bit for pixel";
-		exit(-1);}
+	if (picture.B!='B' || picture.M!='M')
+	{ cout<<"File isn't BitMap! Please enter picture in format .bmp\n";
+	exit(-1);}
+	if (picture.biBitCount!=24)
+	{cout<<"File isn't 24 bit per pixel!\n";
+	exit(-1);}
 
 	long line = (picture.biWidth * 3) + (picture.biWidth % 4);//длина строки с учетов выравнивания
-
 	char * buffer = (char *) calloc((picture.biHeight * line), sizeof(char));//выделим память под буффер
-	
+	int alltemp = line * picture.biHeight;//количество бит в картинке
+
 	//блок считывания растрового массива
 	fseek (stream, picture.bfOffBits, 0);
-	fread (buffer, sizeof(char), (line * picture.biHeight), stream);
+	fread (buffer,(sizeof(char)), alltemp, stream);
 	fclose (stream);
 	
-	//блок создания рисунка
-	FILE *outfile = fopen("D:\outfile.bmp", "w+");
+	//Фильтр "Инверсия"
+	for (int i=0; i<alltemp; i++)
+			buffer[i] = 255-buffer[i];
+	
+	//блок записи отформатированного изображения в файл
+	cout<<"Please, enter the path to picture with inversion in format .bmp: ";
+	char* outfile_name;
+	outfile_name = (char *) calloc(256, sizeof(char));//выделим память под имя
+	cin>>outfile_name;
+	FILE *outfile = fopen(outfile_name, "w+");
+	free(outfile_name);//очистим память взятую под имя
 	fwrite(&picture, sizeof(BMP), 1, outfile);
-	fseek (outfile, picture.bfOffBits, 0);
-	fwrite(buffer, sizeof(char) , (line * picture.biHeight), outfile);
+	fwrite(buffer, sizeof(char), alltemp, outfile);
 	fclose (outfile);
 
 	free(buffer);//очистим память взятую под буффер
-
-	cout<<"Type: "<<picture.B<<picture.M<<endl<<"Width: "<<picture.biWidth<<endl<<"Height: "<<picture.biHeight<<endl<<line;//вывод для проверки
 }
-
 
